fe4d482d72a63ce1661df043ec76b0088bebe30a
McFrase <51033165+McFrase@users.noreply.github.com>
2022-01-25 06:48:06 +0100

    Navbar mobile responsiveness, and added javascript

---
+++ javascript/main.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+    const bar = document.querySelector('.bars');
+    const navContainer = document.querySelector('.navContainer');
+
+    bar.addEventListener("click", function(){
+        this.classList.toggle("active")
+
+        if (this.classList.contains("active")){
+            gsap.to(navContainer, {
+                duration: 1,
+                right: "0%",
+                // ease: "bounce",
+            })
+        }else{
+            gsap.to('.navContainer', {
+                duration: 1,
+                right:"100%",
+            })
+        }
+    })
+}
